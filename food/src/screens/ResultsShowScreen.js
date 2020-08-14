0027d6b2c0752fe2e0183416632b52f0b52baaa7
Winter Gerlach <winter@talixa.com>
2020-08-14 14:15:40 -0400

    Search Screen
    finished Search Screen

---
+++ food/src/screens/ResultsShowScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const ResultsShowScreen = ()=>{
+    return <View>
+        <Text>Screen </Text>
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,21 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
-import useResults from '../hooks/useResults'
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
+
 
 const SearchScreen = () =>{
     const [term, setTerm] = useState('')
     const[searchApi, results, error] = useResults();
-    return <View>
+
+    const filterResultsByPrice=(price)=>{
+        return results.filter(result=>{
+            return result.price === price;
+        });
+    };
+
+    return <>
         <SearchBar 
             term={term} 
             onTermChange={newTerm =>setTerm(newTerm)}
             onTermSubmit={()=>searchApi(term)}
         />
         {error ? <Text>{error}</Text>: null}
-        <Text>We have found {results.length} results</Text>
-    </View>
+        <ScrollView>
+            <ResultsList 
+                title = "Cost Effective"
+                results={filterResultsByPrice('$')}
+            />
+            <ResultsList 
+                title = "Bit Pricier"
+                results={filterResultsByPrice('$$')}
+            />
+            <ResultsList 
+                title = "Big Spender"
+                results={filterResultsByPrice('$$$')}
+            />
+        </ScrollView>
+    </>
 };
 
 const styles = StyleSheet.create({});
